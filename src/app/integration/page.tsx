3d61f26d2539c3bdcb1b48195d08bca1beb82203
uzuz98 <hlb.thanh20091998@gmail.com>
2024-09-24 16:44:15 +0700

    fix

--- src/app/integration/page.tsx
+++ src/app/integration/page.tsx
@@ -57,150 +57,182 @@ const IntegrationScreen = () => {
   }
 
   const handleSendTransaction = async () => {
-    const data = await sendTransaction({
-      from: '0x5a94fF9fefc5B69140375739EB7018AcB0a60cbD',
-      to: '0x5a94fF9fefc5B69140375739EB7018AcB0a60cbD',
-      value: '0x0'
-    })
-    setTxsHash(data)
+    try {
+      const data = await sendTransaction({
+        from: '0x5a94fF9fefc5B69140375739EB7018AcB0a60cbD',
+        to: '0x5a94fF9fefc5B69140375739EB7018AcB0a60cbD',
+        value: '0x0'
+      })
+      setTxsHash(data)
+    } catch (error) {
+      
+    }
   }
 
   const handlePersonalSign = async () => {
-    const data = await personalSign({
-      message: "0x4578616d706c652060706572736f6e616c5f7369676e60206d657373616765",
-      address: address,
-      password: "Example password"
-    })
-    setPersonalSignMsg(data)
+    try {
+      const data = await personalSign({
+        message: "0x4578616d706c652060706572736f6e616c5f7369676e60206d657373616765",
+        address: address,
+        password: "Example password"
+      })
+      setPersonalSignMsg(data)
+    } catch (error) {
+      
+    }
   }
 
   const handleGetEncryptionKey = async () => {
-    const encryptionKey = await getEncryptionKey()
-    console.log("🩲 🩲 => handleGetEncryptionKey => encryptionKey:", encryptionKey)
+    try {
+      const encryptionKey = await getEncryptionKey()
+      console.log("🩲 🩲 => handleGetEncryptionKey => encryptionKey:", encryptionKey)
+    } catch (error) {
+      
+    }
   }
 
   const handleEncryptKey = () => {
-    const data = encryptKey(value)
-    console.log("🩲 🩲 => handleEncryptKey => data:", data)
-    setCipherText(data)
+    try {
+      const data = encryptKey(value)
+      console.log("🩲 🩲 => handleEncryptKey => data:", data)
+      setCipherText(data)
+    } catch (error) {
+        
+    }
   }
 
   const handleDecryptKey = async () => {
-    const data = await decryptKey(cipherText)
-    setClearText(data)
+    try {
+      const data = await decryptKey(cipherText)
+      setClearText(data)
+    } catch (error) {
+      
+    }
   }
 
   const handleSignTypedDataV1 = async () => {
-    const data = await handleSignTypedV1([
-      {
-        "type": "string",
-        "name": "Message",
-        "value": "Hi, Alice!"
-      },
-      {
-        "type": "uint32",
-        "name": "A number",
-        "value": "1337"
-      }
-    ])
-    setSignTypedData(data)
+    try {
+      const data = await handleSignTypedV1([
+        {
+          "type": "string",
+          "name": "Message",
+          "value": "Hi, Alice!"
+        },
+        {
+          "type": "uint32",
+          "name": "A number",
+          "value": "1337"
+        }
+      ])
+      setSignTypedData(data)
+    } catch (error) {
+      
+    }
   }
 
   const handleSignTypedDataV3 = async () => {
-    const data = await signTypedDataV3({
-      types: {
-        EIP712Domain: [
-          { name: "name", type: "string" },
-          { name: "version", type: "string" },
-          { name: "chainId", type: "uint256" },
-          { name: "verifyingContract", type: "address" },
-        ],
-        Person: [
-          { name: 'name', type: 'string' },
-          { name: 'wallet', type: 'address' },
-        ],
-        Mail: [
-          { name: 'from', type: 'Person' },
-          { name: 'to', type: 'Person' },
-          { name: 'contents', type: 'string' },
-        ],
-      },
-      primaryType: 'Mail',
-      domain: {
-        name: 'Ether Mail',
-        version: '1',
-        chainId: Number(chainId),
-        verifyingContract: '0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC',
-      },
-      message: {
-        from: {
-          name: 'Cow',
-          wallet: '0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826',
+    try {
+      const data = await signTypedDataV3({
+        types: {
+          EIP712Domain: [
+            { name: "name", type: "string" },
+            { name: "version", type: "string" },
+            { name: "chainId", type: "uint256" },
+            { name: "verifyingContract", type: "address" },
+          ],
+          Person: [
+            { name: 'name', type: 'string' },
+            { name: 'wallet', type: 'address' },
+          ],
+          Mail: [
+            { name: 'from', type: 'Person' },
+            { name: 'to', type: 'Person' },
+            { name: 'contents', type: 'string' },
+          ],
         },
-        to: {
-          name: 'Bob',
-          wallet: '0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB',
+        primaryType: 'Mail',
+        domain: {
+          name: 'Ether Mail',
+          version: '1',
+          chainId: Number(chainId),
+          verifyingContract: '0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC',
         },
-        contents: 'Hello, Bob!',
-      },
-    })
-    setSignTypedDataV3Res(data)
+        message: {
+          from: {
+            name: 'Cow',
+            wallet: '0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826',
+          },
+          to: {
+            name: 'Bob',
+            wallet: '0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB',
+          },
+          contents: 'Hello, Bob!',
+        },
+      })
+      setSignTypedDataV3Res(data)
+    } catch (error) {
+      
+    }
   }
 
   const handleSignTypedDataV4 = async () => {
-    const data = await signTypedDataV4({
-      domain: {
-        chainId: Number(chainId),
-        name: 'Ether Mail',
-        verifyingContract: '0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC',
-        version: '1',
-      },
-      message: {
-        contents: 'Hello, Bob!',
-        from: {
-          name: 'Cow',
-          wallets: [
-            '0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826',
-            '0xDeaDbeefdEAdbeefdEadbEEFdeadbeEFdEaDbeeF',
-          ],
+    try {
+      const data = await signTypedDataV4({
+        domain: {
+          chainId: Number(chainId),
+          name: 'Ether Mail',
+          verifyingContract: '0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC',
+          version: '1',
         },
-        to: [
-          {
-            name: 'Bob',
+        message: {
+          contents: 'Hello, Bob!',
+          from: {
+            name: 'Cow',
             wallets: [
-              '0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB',
-              '0xB0BdaBea57B0BDABeA57b0bdABEA57b0BDabEa57',
-              '0xB0B0b0b0b0b0B000000000000000000000000000',
+              '0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826',
+              '0xDeaDbeefdEAdbeefdEadbEEFdeadbeEFdEaDbeeF',
             ],
           },
-        ],
-        attachment: '0x',
-      },
-      primaryType: 'Mail',
-      types: {
-        EIP712Domain: [
-          { name: "name", type: "string" },
-          { name: "version", type: "string" },
-          { name: "chainId", type: "uint256" },
-          { name: "verifyingContract", type: "address" },
-        ],
-        Group: [
-          { name: 'name', type: 'string' },
-          { name: 'members', type: 'Person[]' },
-        ],
-        Mail: [
-          { name: 'from', type: 'Person' },
-          { name: 'to', type: 'Person[]' },
-          { name: 'contents', type: 'string' },
-          { name: 'attachment', type: 'bytes' },
-        ],
-        Person: [
-          { name: 'name', type: 'string' },
-          { name: 'wallets', type: 'address[]' },
-        ],
-      }
-    })
-    setSignTypedDataV4Res(data)
+          to: [
+            {
+              name: 'Bob',
+              wallets: [
+                '0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB',
+                '0xB0BdaBea57B0BDABeA57b0bdABEA57b0BDabEa57',
+                '0xB0B0b0b0b0b0B000000000000000000000000000',
+              ],
+            },
+          ],
+          attachment: '0x',
+        },
+        primaryType: 'Mail',
+        types: {
+          EIP712Domain: [
+            { name: "name", type: "string" },
+            { name: "version", type: "string" },
+            { name: "chainId", type: "uint256" },
+            { name: "verifyingContract", type: "address" },
+          ],
+          Group: [
+            { name: 'name', type: 'string' },
+            { name: 'members', type: 'Person[]' },
+          ],
+          Mail: [
+            { name: 'from', type: 'Person' },
+            { name: 'to', type: 'Person[]' },
+            { name: 'contents', type: 'string' },
+            { name: 'attachment', type: 'bytes' },
+          ],
+          Person: [
+            { name: 'name', type: 'string' },
+            { name: 'wallets', type: 'address[]' },
+          ],
+        }
+      })
+      setSignTypedDataV4Res(data)
+    } catch (error) {
+      
+    }
   }
 
   return (
