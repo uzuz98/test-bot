643967a331eb8aa27b664d4d8be2645754a024ed
uzuz98 <hlb.thanh20091998@gmail.com>
2024-09-24 11:42:45 +0700

    fix cors

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,16 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   reactStrictMode: false,
-  headers:async () => [
+  headers: async () => [
+    {
+      source: "/api/token",
+      headers: [
+        { key: "Access-Control-Allow-Credentials", value: "true" },
+        { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+        { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+        { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" }
+      ],
+    },
     {
       source: "/hello-world",
       headers: [
@@ -29,20 +38,6 @@ const nextConfig = {
           key: "Access-Control-Allow-Methods",
           value: "GET, POST, PUT, DELETE, OPTIONS",
         }
-      ],
-    },
-    {
-      source: "/api/token",
-      headers: [
-        {
-          key: "Access-Control-Allow-Origin",
-          value: "*", // Set your origin
-        },
-        {
-          key: "Access-Control-Allow-Methods",
-          value: "GET, POST, PUT, DELETE, OPTIONS",
-        },
-        { key: "Access-Control-Allow-Credentials", value: true },
       ],
     }
   ]
